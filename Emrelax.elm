af6ee6dc8ae23cabf77fb519e03083894bd764d4
Mabry Cervin <mpcervin@uncg.edu>
2015-02-05 16:52:34 -0500

    Initial commit

---
+++ Emrelax.elm
@@ -0,0 +1,21 @@
+import Graphics.Element (..)
+import Signal (Signal)
+import Text
+import Time
+import Window
+import List (head, tail, (::))
+
+-- Models
+type alias Node = {charge:Float}
+type alias Zipper = {front:List Node, focus:Node, back:List Node}
+
+mvForward : Zipper -> Zipper
+mvForward x =
+  let
+    focus' = head x.front
+    front' = tail x.front
+    back' = x.focus::x.back
+  in
+    {front = front', focus = focus', back = back'}
+
+
